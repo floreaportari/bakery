9dcf36e6c2403dbdfbced14bd6f10e6fa0116a5a
Florian Portari <floryanportar@gmail.com>
2022-07-25 15:23:41 +0200

    removed app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector("#menuBtn");
-const navItems = document.querySelector("#navItems");
-
-menuBtn.addEventListener("click", function () {
-  navItems.classList.toggle("show");
-});
